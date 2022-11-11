d38691b0abf4a7338c901b262ca5e6f89a9b43d0
giomikadze <110746808+giomikadze@users.noreply.github.com>
2022-11-11 16:40:06 +0100

    lessoln18

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+let number = 5;
+function plus(){
+  if (number < 50){
+    number +=5;
+  }
+  document.getElementById('number').innerHTML= number;
+}
+
+function minus(){
+  if(number > 0 && number <=50 ){
+    number -=5;
+  }
+  document.getElementById('number').innerHTML = number;
+}
+
+
+
+
+
+
+
